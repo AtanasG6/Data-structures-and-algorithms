5f2b943972955e854cc84984b8a673dd6ca2dd3d
AtanasG6 <gyulchev666@gmail.com>
2024-04-21 17:36:11 +0300

    Linked Queue

---
+++ LinkedQueue/LinkedQueue/CustomQueue.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace LinkedQueue
+{
+	class CustomQueue<T>
+	{
+		public int Count { get; private set; }
+
+		public QueueNode<T> Head { get; set; }
+
+		public QueueNode<T> Tail { get; set; }
+
+		public void Enqueue(T element)
+		{
+			var node = new QueueNode<T>(element);
+			if (Count == 0)
+			{
+				Head = Tail = node;
+			}
+			else
+			{
+				Tail.Next = node;
+				node.Prev = Tail;
+				Tail = node;
+			}
+
+			Count++;
+		}
+
+		public T Dequeue()
+		{
+			if (Count == 0)
+			{
+				throw new InvalidOperationException("The queue is empty.");
+			}
+			else
+			{
+				var removedElement = Head.Value;
+				Head = Head.Next;
+
+				Count--;
+				return removedElement;
+			}
+			
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			var element = Head;
+			while (element != null)
+			{
+				sb.Append(element.Value + " ");
+				element = element.Next;
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ LinkedQueue/LinkedQueue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace LinkedQueue
+{
+	 class Program
+	{
+		static void Main()
+		{
+			var q = new CustomQueue<int>();
+			q.Enqueue(1);
+			q.Enqueue(2);
+			q.Enqueue(3);
+			q.Enqueue(4);
+			Console.WriteLine(q.Dequeue());
+			Console.WriteLine(q.ToString());
+		}
+
+	}
+}
