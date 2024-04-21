6301d8a9ee0bcada31ea483d3be2e464a0c00d1f
AtanasG6 <gyulchev666@gmail.com>
2024-04-21 17:15:08 +0300

    Array Stack

---
+++ ArrayStack/ArrayStack/CustomStack.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace ArrayStack
+{
+	public class CustomStack<T>
+	{
+		private T[] items;
+		public int Count { get; private set; }
+
+		private const int INITIAL_CAPACITY = 4;
+
+		public CustomStack(int size = INITIAL_CAPACITY)
+		{
+			items = new T[size];
+			Count = 0;
+		}
+
+		public void Push(T element)
+		{
+			if (items.Length == Count)
+			{
+				Array.Resize(ref items, Count * 2);
+			}
+
+			items[Count++] = element;
+		}
+
+		public T Pop()
+		{
+			if (Count == 0)
+			{
+				throw new InvalidOperationException("The stack is empty.");
+			}
+
+			Count--;
+			var elementToDelete = items[Count];
+			items[Count] = default(T);
+			return elementToDelete;
+		}
+
+		public T Peek()
+		{
+			if (Count == 0)
+			{
+				throw new InvalidOperationException("The stack is empty.");
+			}
+
+			return items[Count-1];
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+
+			for (int i = 0; i < Count; i++)
+			{
+				sb.Append(items[i] + " ");
+			}
+			
+			return sb.ToString();
+		}
+	}
+}
---
+++ ArrayStack/ArrayStack/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ArrayStack
+{
+	 class Program
+	{
+		static void Main()
+		{
+			CustomStack<int> stack = new CustomStack<int>(2);
+			stack.Push(1);
+			stack.Push(2);
+			stack.Push(3);
+			stack.Push(4);
+			stack.Push(5);
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Peek());
+			Console.WriteLine(stack.ToString());
+			
+		}
+	}
+}
