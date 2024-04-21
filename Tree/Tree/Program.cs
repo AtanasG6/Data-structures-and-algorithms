16c303ad76d17822ef04651b3b6ce734bd400782
AtanasG6 <gyulchev666@gmail.com>
2024-04-21 18:08:55 +0300

    Tree, DFS, BFS

---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tree
+{
+	class Program
+	{
+		static void Main()
+		{
+			var tree = new Tree<int>(1, 
+				          new Tree<int>(2, 
+					          new Tree<int>(5, 
+						          new Tree<int>(6))),
+				                        new Tree<int>(3, 
+					                        new Tree<int>(4)));
+			//tree.DFSRecursive();
+			//tree.TraverseDFS();
+			tree.TraverseBFS();
+		}
+	}
+}
---
+++ Tree/Tree/Tree.cs
@@ -0,0 +1,64 @@
+﻿namespace Tree
+{
+	public class Tree<T>
+	{
+		public TreeNode<T> Root { get; private set; }
+
+		public Tree(T value, params Tree<T>[] subtrees)
+		{
+			Root = new TreeNode<T>(value);
+			foreach (var subtree in subtrees)
+			{
+				Root.Children.Add(subtree.Root);
+			}
+		}
+
+		public void DFSRecursive()
+		{
+			DFSRecursive(Root);
+		}
+
+		private void DFSRecursive(TreeNode<T> root)
+		{
+			Console.WriteLine(root.Value);
+			foreach (var child in root.Children)
+			{
+				DFSRecursive(child);
+			}
+		}
+
+		public void TraverseDFS()
+		{
+			var stack = new Stack<TreeNode<T>>();
+			stack.Push(Root);
+
+			while (stack.Count > 0)
+			{
+				var currentNode = stack.Pop();
+				Console.WriteLine(currentNode.Value);
+
+				foreach (var child in currentNode.Children)
+				{
+					stack.Push(child); 
+				}
+			}
+		}
+
+		public void TraverseBFS()
+		{
+			var q = new Queue<TreeNode<T>>();
+			q.Enqueue(Root);
+
+			while (q.Count > 0)
+			{
+				var currentNode = q.Dequeue();
+				Console.WriteLine(currentNode.Value);
+
+				foreach (var child in currentNode.Children)
+				{
+					q.Enqueue(child);
+				}
+			}
+		}
+	}
+}
